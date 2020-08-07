646af6b4c38c00c014b5dd1ed6e0cc23b6e55dd7
ariela <ariela.haley65@gmail.com>
2020-08-07 03:16:33 -0700

    automating ansible-wip

---
+++ terraform/jenkins/inventory.tpl
@@ -0,0 +1,2 @@
+[jenkins]
+ubuntu@${host_dns} ansible_user=ubuntu ansible_ssh_key_private_file=${key_path}
--- terraform/jenkins/main.tf
+++ terraform/jenkins/main.tf
@@ -2,6 +2,25 @@ provider "aws" {
     region = "us-east-2"
 }
 
+data "template_file" "inventory" {
+    template = "${file("inventory.tpl")}"
+
+    vars = {
+        host_dns = "${aws_instance.jenkins_instance.public_dns}"
+        key_path = "${var.ssh_key_private}"
+    }
+}
+resource "null_resource" "update_inventory" {
+
+    triggers = {
+        template = "${data.template_file.inventory.rendered}"
+    }
+
+    provisioner "local-exec" {
+        command = "echo '${data.template_file.inventory.rendered}' > ../../ansible/inventory"
+    }
+}
+
 resource "aws_instance" "jenkins_instance" {
     ami = "ami-0a63f96e85105c6d3"
     instance_type = "t2.micro"
@@ -11,21 +30,20 @@ resource "aws_instance" "jenkins_instance" {
         Name = "${var.name}"
     }
     provisioner "remote-exec" {
-    # Install Python for Ansible
+
     connection {
       type        = "ssh"
       user        = "ubuntu"
       private_key = file(var.ssh_key_private)
-      host        = aws_instance.jenkins_instance.public_ip
+      host        = aws_instance.jenkins_instance.public_dns
       timeout = "500s"
     }
-      working_dir = "../../ansible"
-      inline = ["sudo ansible-playbook jenkins.yaml"]
+
   }
 
-  # provisioner "local-exec" {
-  #   working_dir = "../../ansible"
-  #   # command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.ssh_key_private} jenkins.yaml"
-  #   command = "ansible-playbook -u ubuntu -i ‘${aws_instance.jenkins_instance.public_dns},’ jenkins.yaml"
-  # }
+  provisioner "local-exec" {
+    working_dir = "../../ansible"
+    # command = "sleep 60; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.ssh_key_private} -i '${aws_instance.jenkins_instance.public_dns},' jenkins.yaml"
+    command = "ansible-playbook -i inventory jenkins.yaml"
+  }
 }
