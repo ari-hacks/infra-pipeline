c8bab0a4221a977cd05a3b552bb64ac3c2d1be73
ariela <ariela.haley65@gmail.com>
2020-08-06 21:48:42 -0700

    automating ansible

--- terraform/jenkins/main.tf
+++ terraform/jenkins/main.tf
@@ -10,4 +10,21 @@ resource "aws_instance" "jenkins_instance" {
     tags = {
         Name = "${var.name}"
     }
+  #   provisioner "remote-exec" {
+  #   # Install Python for Ansible
+  #   #inline = ["sudo dnf -y install python"]
+
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "ubuntu"
+  #     private_key = file(var.ssh_key_private)
+  #     host        = aws_instance.jenkins_instance.public_ip
+  #   }
+  # }
+
+  # provisioner "local-exec" {
+  #   working_dir = "../../ansible"
+  #   command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ${var.ssh_key_private} jenkins.yaml"
+
+  # }
 }
--- terraform/jenkins/variables.tf
+++ terraform/jenkins/variables.tf
@@ -1,3 +1,7 @@
 variable "name" {
     description = "Name the instance on deploy"
 }
+variable "ssh_key_private" {
+  default     = "~/.ssh/infra-key.pem"
+  description = "Path to the SSH"
+}
